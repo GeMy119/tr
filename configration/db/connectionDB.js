49775c724f10566f2f864eb3eb2e077f7d81230f
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-10-20 14:16:19 +0300

    added mongo link

--- configration/db/connectionDB.js
+++ configration/db/connectionDB.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 
 const connection = () => {
-    return mongoose.connect("mongodb://127.0.0.1:27017/tasksDB").then((result) => {
+    return mongoose.connect("mongodb+srv://trelloApp:trelloApp@atlascluster.zgoi85r.mongodb.net/").then((result) => {
         console.log("connected DB done")
     }).catch((err) => {
         console.log("error", err)
