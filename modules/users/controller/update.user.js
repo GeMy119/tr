601736bf3ea47d12c56ea4a86bb336373650e23e
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-10-22 15:18:25 +0300

    updated user joi validation

--- modules/users/controller/update.user.js
+++ modules/users/controller/update.user.js
@@ -1,19 +1,26 @@
 const User = require("../schema/user.schema");
-const statusCodes = require("http-status-codes");
+const { OK, BAD_REQUEST, INTERNAL_SERVER_ERROR } = require("http-status-codes");
+
 const updateUser = async (req, res) => {
     try {
         const uData = {
             userName: req.body.userName,
             age: req.body.age,
-            phone:req.body.phone
+            phone: req.body.phone
         }
-        const userId = req.parmas.id
-        const userDateUpdated = await User.findByIdAndUpdate(userId, uData, { new: true })
-        res.status(statusCodes.OK).json({ message: "user data updated", userDateUpdated })
-    } catch (error) {
-        res.status(statusCodes.BAD_REQUEST).json({ message: "error", error })
+        const userId = req.params.id;
 
+        const userDateUpdated = await User.findByIdAndUpdate(userId, uData, { new: true });
+
+        if (!userDateUpdated) {
+            return res.status(BAD_REQUEST).json({ message: "User not found or update failed" });
+        }
+
+        res.status(OK).json({ message: "User data updated", userDateUpdated });
+    } catch (error) {
+        console.error(error);
+        res.status(INTERNAL_SERVER_ERROR).json({ message: "Internal server error", error: error.message });
     }
 }
 
-module.exports = updateUser
+module.exports = updateUser;
