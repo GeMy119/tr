ef281d85c17aef08a97c67f25dd7e5af1060f92c
GeMy119 <76409042+GeMy119@users.noreply.github.com>
2023-10-24 19:43:05 +0300

    update in task schema

--- modules/taska/schema/tasks.schema.js
+++ modules/taska/schema/tasks.schema.js
@@ -5,7 +5,7 @@ const tasksSchema = new mongoose.Schema({
     des: { type: String, required: true },
     status: { type: String, enum: ["toDo", "doing", "done"], default: "toDo" },
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-    asignTo: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
+    // asignTo: { type: mongoose.Schema.Types.ObjectId, ref: "users", required: true },
     deadline: { type: Date, required: true }
 },
     {
